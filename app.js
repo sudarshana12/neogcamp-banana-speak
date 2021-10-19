5e79e3492705cc0a869b0a1bdad8e79a366ec62a
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-10-19 14:17:20 +0530

    js imported

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("script is working from a different file");
+
+//input
+var username = prompt("Give me username");
+
+//processing 
+var welcomeMessage = "this script works!!" + username;
+
+//output
+alert(welcomeMessage);
