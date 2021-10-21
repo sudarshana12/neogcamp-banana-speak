713fa101e7d0cfbf688e5edae6c16c7b25d8a855
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-10-21 15:28:24 +0530

    error handling

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text){
     return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured",error)
+    alert("Something wrong with server! Try again after sometime")
 }
 function clickHandler(){
     var inputText = txtInput.value; //taking input
@@ -12,7 +19,11 @@ function clickHandler(){
     // calling server for processing
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
-        .then(json => console.log(json.contents.translated))
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
